b9344e7f2dd7486d32818901cca1b3842a1e230c
kprichard <kprichard@gmail.com>
2024-09-20 20:08:40 -0700

    - test_completeness.py: finish gumball machine tests

--- tests/test_completeness.py
+++ tests/test_completeness.py
@@ -1,6 +1,7 @@
 import logging
 from unittest import TestCase
 
+from examples.gumball_machine import *
 from lean import (
     StateMachine, State, StateException, TransitionException,
     StateMachineException)
@@ -278,21 +279,58 @@ class TestNormalMachineWithAllEvents(TestCase):
             self.assertEqual(sm._events_called[idx], event)
 
 
-from examples.gumball_machine import *
+######################################################################
+
 
 class TestGumballStateMachine(TestCase):
     def setUp(self):
-        self.gmh = GumballMachineHardware()
-        self.gsm = GumballStateMachine(
-            name="Gumball machine controller",
-            desc="Demo of a gumball machine",
-            model=self.gmh)
+        # Get a gumball machine hardware API
+        self.gumball_hw = GumballMachineHardware()
+
+        # Create a gumball machine state machine with the hardware API as context
+        self.gumball_sm = GumballStateMachine(
+            name="Gumball state machine controller",
+            desc="Demo of a gumball machine controlled by lean-statemachine",
+            model=self.gumball_hw)
 
     def test_ready(self):
-        self.assertEqual(self.gsm.state, GumballStateMachine.ready)
+        self.assertEqual(self.gumball_sm.state,
+                         GumballStateMachine.ready)
 
     def test_coin_insert(self):
-        self.assertEqual(self.gsm.state, GumballStateMachine.ready)
-        self.gmh.coin_slot(COIN_DROP)
-        self.gsm.cycle()
-        self.assertEqual(self.gsm.state, GumballStateMachine.coin_dropped)
+        # the just-instantiated gumball machine should be in the ready state
+        self.assertEqual(self.gumball_sm.state,
+                         GumballStateMachine.ready)
+
+        # Simulate the user dropping a coin, by telling the gumball hardware
+        self.gumball_hw.coin_slot(COIN_DROP)
+
+        # Cycle the state machine, which causes it to check the hardware API's new state
+        self.gumball_sm.cycle()
+
+        # The
+        self.assertEqual(self.gumball_sm.state,
+                         GumballStateMachine.coin_dropped)
+
+        # User turns the crank ...
+        for degree in (90, 180, 270, 360):
+            self.gumball_hw.turn_crank(degree)
+            self.gumball_sm.cycle()
+
+            # check that the gumball has NOT dropped
+            if degree < 360:
+                self.assertEqual(self.gumball_sm.state,
+                                 GumballStateMachine.crank_turned)
+
+        # When the crank has spun around 360 degrees, the gumball will drop
+        self.assertEqual(self.gumball_sm.state,
+                         GumballStateMachine.gumball_dispensed)
+
+        self.gumball_sm.cycle()
+
+        self.assertEqual(self.gumball_sm.state,
+                         GumballStateMachine.crank_returned)
+
+        # No further advancement possible as state 'crank_returned' is final,
+        # so .cycle() will return False
+        self.assertFalse(self.gumball_sm.cycle())
